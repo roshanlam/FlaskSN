82e413ad8039c5c54c9a1b8728aa6de65c7b55a6
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-12-26 19:28:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config('SQLAlchemy_DATABASE_URL") = '' # Need to put link ClearDB Database (or any other Database)
   db = SQLAlchemy(app)         
      
-@app.route('/')
+           Class User(db.Model):
+                 id = db.Column(db.Integer, primary_key=True)
+                 username = db.Column(db.String(100), nullable=False, unique=True)
+                 email = db.Column(db.String(120), nullable=False, unique=True)
+                 password = db.Column(db.String(120), nullable=False)
+                 date_created = db.Column(db.DateTime, default=datetime.utcnow) 
 
+@app.route('/')
     def index():
         return 'hello world'
 
 if __name__ == '__main__':
   app.run(debug=True)
-  
+ 
